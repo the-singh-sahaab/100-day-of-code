9b97e5b73b16d8a2951ae604ebf5fec8ba2fc1d9
Shubham Singh <105417139+the-singh-sahaab@users.noreply.github.com>
2023-10-13 23:57:45 +0530

    Add files via upload

---
+++ day_8/rojka.py
@@ -0,0 +1,16 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        suffix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
+
+        return res
+        
