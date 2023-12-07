34d0b95eb077a6422a29978300c9a8286156cb76
Shubham Singh <105417139+the-singh-sahaab@users.noreply.github.com>
2023-12-08 01:24:08 +0530

    Add files via upload

--- day_60/rojka.py
+++ day_60/rojka.py
@@ -1,9 +1,14 @@
 class Solution:
-    def countDistinctIntegers(self, nums: List[int]) -> int:
-        new = []
-        for i in nums:
-            new.append(int(str(i)[::-1]))
-        # print(nums+new)
-        return len(list(set(nums+new)))
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+
+        D = defaultdict(int)
+
+        for x in nums:
+            D[x] += 1
+
+        for x in sorted(nums):
+            if D[x] > 0:
+                for i in range(k):
+                    D[x + i] -= 1
         
-        
+        return len(set(list(D.values()))) == 1
